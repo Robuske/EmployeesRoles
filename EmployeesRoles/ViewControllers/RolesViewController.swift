17f857e8f6fb103915c3d72230efbd3f29c9b719
Rodrigo Cardoso Buske <rodbuske@gmail.com>
2017-08-22 20:15:23 -0300

    Created viewcontrollers

---
+++ EmployeesRoles/ViewControllers/RolesViewController.swift
@@ -0,0 +1,42 @@
+//
+//  RolesViewController.swift
+//  EmployeesRoles
+//
+//  Created by Rodrigo Cardoso Buske on 22/08/17.
+//  Copyright © 2017 Buske Org. All rights reserved.
+//
+
+import UIKit
+
+class RolesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+	
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 0
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		return UITableViewCell()
+	}
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
